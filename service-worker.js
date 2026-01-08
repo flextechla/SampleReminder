5d0fd8714f0b15e59f376911a4837e6581051ea4
flextechla <don@flextechla.com>
2026-01-07 18:00:25 -0600

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'sampleflow-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch assets from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
